f59c260af3439f3aefb0842659e44e8ea3bfbe00
Giulio Yongco <yongcogiulio125@gmail.com>
2024-04-25 10:18:49 +0800

    graph init

---
+++ 04_graphs/adjecency_list.h
@@ -0,0 +1,41 @@
+// =======================================================================
+// Description     : Code that will impress u ;)
+// Author          : G.M. Yongco #BeSomeoneWhoCanStandByShinomiya
+// Date            : ur my date uwu
+// =======================================================================
+
+#include <stdio.h>
+#include <string.h>
+
+// =======================================================================
+// MISC FUNCTIONS
+// =======================================================================
+
+void separator(char* message){
+	int count = 55; 				//additional 5 for the (\n)s and the \0
+	char new_string[count];
+
+	memset(new_string, '-', count);
+
+	new_string[0] = '\n';
+	new_string[1] = '\n';
+	new_string[count-2] = '\n';
+	new_string[count-3] = '\n';
+	new_string[count-1] = '\0';
+
+	memcpy(new_string + 2, message, strlen(message));
+	new_string[strlen(message) + 2] = ' ';
+
+	printf("%s", new_string);
+}
+
+// =======================================================================
+// SHORTEST PATH ALGOS
+// =======================================================================
+
+void dijkstra();
+void warshall();
+
+// =======================================================================
+// SHORTEST PATH ALGOS
+// =======================================================================
